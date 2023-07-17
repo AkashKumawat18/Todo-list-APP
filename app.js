8acc4fc06290dda5cc26eaf529203f5992f5eb13
Akash Kumawat <kashu7102@gmail.com>
2023-07-17 18:37:21 +0530

    change port settings

--- app.js
+++ app.js
@@ -139,7 +139,7 @@ app.post("/delete", function (req, res) {
       });
   }
 });
-
-app.listen(8000, () => {
-  console.log("Server is working on 8000");
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is working on ${PORT}`);
 });
